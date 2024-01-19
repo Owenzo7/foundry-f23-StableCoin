6208623bd58389cc32887b95e3aa757b42db742e
Owenzo7 <owenlee545@gmail.com>
2024-01-19 16:26:50 +0300

    now want to create a deposit and mint function

--- test/unit/DscEngineTest.t.sol
+++ test/unit/DscEngineTest.t.sol
@@ -7,6 +7,7 @@ import {Deploydsc} from "../../script/DeployDsc.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract DscEngineTest is Test {
     Deploydsc deployer;
@@ -16,10 +17,17 @@ contract DscEngineTest is Test {
     address ethUsdPriceFeed;
     address weth;
 
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    address public USER = makeAddr("user");
+
     function setUp() public {
         deployer = new Deploydsc();
         (dsc, dsce, config) = deployer.run();
         (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     ///////////////////////////////
@@ -32,5 +40,17 @@ contract DscEngineTest is Test {
 
         uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
         assertEq(expectedUsd, actualUsd);
+    }
+
+    ///////////////////////////////
+    /// DepositCollateral Test //////////
+
+    function testRevertIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approveInternal(address(dsce), USER, AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }
