a50044dbc683eb8cf985d49a4740f3d0639686b5
Owenzo7 <owenlee545@gmail.com>
2024-01-29 15:00:51 +0300

    made some invariant tests

--- test/Invariant/InvariantTest.t.sol
+++ test/Invariant/InvariantTest.t.sol
@@ -6,20 +6,46 @@
 
 pragma solidity ^0.8.17;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {Deploydsc} from "../../script/DeployDsc.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "./Handler.t.sol";
 
 contract invariantTest is StdInvariant, Test {
     Deploydsc deployer;
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
+    HelperConfig config;
+    Handler handler;
+    address public weth;
+    address public wbtc;
 
     function setUp() external {
         deployer = new Deploydsc();
-        dsce = deployer.run();
+        (dsc, dsce, config) = deployer.run();
+
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalBtcDeposited);
+
+        console.log("weth value: ", wethValue);
+        console.log("wbtc value: ", wbtcValue);
+        console.log("total supply: ", totalSupply);
+
+        assert(wethValue + wbtcValue >= totalSupply);
     }
 }
