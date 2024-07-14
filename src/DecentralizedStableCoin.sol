0b3492839687ba47452265b56eef507602519a78
Owen Lee <lee@Owens-MacBook-Air.local>
2024-07-14 19:41:15 +0300

    added some audit tags

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volatility coin
 
 // Layout of Contract:
 // version
@@ -38,8 +38,6 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 *
 *
 * This is the contract meant to be governed by DScEngine. This contract is just the ERC20 implementation of our stablecoin system
-
-
 */
 
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
@@ -47,31 +45,50 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
+
+
+// @audit-ok --> Just naming the StableCoin
     constructor() ERC20("DecentralizedStableCoin", "DSC") {}
 
+
+    // @audit-ok --> Access control is fine
+    // @audit-ok --> Overall function is fine.
     function burn(uint256 _amount) public override onlyOwner {
+        // @audit-ok --> checking the amount of stableCoin on user
         uint256 balance = balanceOf(msg.sender);
+
+        // @audit-ok --> Less than zero check valid.
         if (_amount <= 0) {
             revert DecentralizedStableCoin__MustBeMoreThanZero();
         }
+        // @audit-ok --> less balance check valid.
         if (balance < _amount) {
             revert DecentralizedStableCoin__BurnAmountExceedsBalance();
         }
 
+        // @audit-ok
+        // Super means use the burn function from the parent class which is (ERC20Burnable.)
+        // Used because the burn function is being overridden.
         super.burn(_amount);
     }
 
+    // @audit-ok --> access control valid.
+    // @audit-ok --> function is overally valid.
     function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        // @audit-ok --> zero address check is valid..
         if (_to == address(0)) {
             revert DecentralizedStableCoin__NotZeroAddress();
         }
 
+        // @audit-ok --> amount less than zero check valid..
         if (_amount <= 0) {
             revert DecentralizedStableCoin__MustBeMoreThanZero();
         }
 
+        // @audit-ok --> function choice valid
         _mint(_to, _amount);
 
+        // @audit-ok --> return valid.
         return true;
     }
 }
